1186680cf9112559c95f36825013566e076fddff
Vani Vasudevan <vanivasudevan@users.noreply.github.com>
2025-11-21 12:02:08 +0530

    Update student_enrollment.py

--- Pages/student_enrollment.py
+++ Pages/student_enrollment.py
@@ -1,1 +1,6 @@
-
+mport streamlit as st
+st.title('Student Enrollment Page')
+subjects=st.multiselect('Select Subjects',['Maths','Science','Physics','Chemistry',
+'English'])
+st.write('Selected Subjects : ',subjects)
+ 
