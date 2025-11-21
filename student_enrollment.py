b24ac4de2b13029fde1a6ef902146b025516dc24
Vani Vasudevan <vanivasudevan@users.noreply.github.com>
2025-11-21 12:03:27 +0530

    Delete student_enrollment.py

--- student_enrollment.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title('Student Enrollment Page')
-subjects=st.multiselect('Select Subjects',['Maths','Science','Physics','Chemistry',
-'English'])
-st.write('Selected Subjects : ',subjects)
- 
